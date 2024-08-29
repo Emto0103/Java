432d0c1fb0e4b93f90aa74cfc4dfc76402084894
emto0103 <emto0103@gmail.com>
2024-08-29 11:56:07 +0900

    0829

--- src/cat/Main.java
+++ src/cat/Main.java
@@ -8,15 +8,15 @@ public class Main {
         cat.setName("AICat");
         cat.setBreeds("Persian");
         cat.setWeight(3.2);
-//
+
         System.out.printf("고양이 이름 : %s\n", cat.getName());
         System.out.printf("고양이 품종 : %s\n", cat.getBreeds());
         System.out.printf("고양이 무게 : %.2f\n", cat.getWeight());
 
-//        System.out.print(cat.getName()+"이 ");
-//        cat.claw();
-//
-//        System.out.print(cat.getName()+"이 ");
-//        cat.meow();
+        System.out.print(cat.getName()+"이 ");
+        cat.claw();
+
+        System.out.print(cat.getName()+"이 ");
+        cat.meow();
     }
 }
